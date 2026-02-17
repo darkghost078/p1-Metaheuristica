881caf0927dddd39c692eb7ae5064df59e7d5065
PMMS22 <pablo.m.martinsegovia@gmail.com>
2026-02-17 13:16:29 +0100

    Ahora si funciona

--- puntosRandom.py
+++ puntosRandom.py
@@ -1,17 +1,16 @@
 import random
 
 def puntuakAusazko(a,n):
-    n2 = n - 1
     b = []
-    while(n2>1):
+    while(n>1):
         r = random.randint(1,len(a)-1)
         if r not in b:
             b.append(r)
-            n2 = n2 - 1
+            n = n - 1
     b.sort()
     print(b)
 
-a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+a = [0, 73748659823454631, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 n = int(input("Introduce el numero de puntos: "))
 
 puntuakAusazko(a,n)
