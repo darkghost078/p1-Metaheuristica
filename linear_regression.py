849cb645f602d6c4ac3a953a6b8179777fc0be88
Darío Martínez Kostyuk <dariomartk@gmail.com>
2026-02-17 17:40:19 +0100

    feat: tryed and fixed some things on linear_regression

--- linear_regression.py
+++ linear_regression.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 def estimate_coef(x, y):
+    x = np.array(x)
+    y = np.array(y)
     n = np.size(x)
 
     m_x = np.mean(x)
@@ -13,16 +15,18 @@ def estimate_coef(x, y):
     b_0 = SS_xy / SS_xx
     b_1 = m_y - b_0 * m_x
 
-    return (b_0, b_1)
+    return (b_0.item(), b_1.item())
 
 
 def estimate_all_coef(temp, points):
     coef = []
     start = 0
-    points.append(temp.size())
 
-    for point in points:
-        b_0, b_1 = estimate_coef(range(start, point), coef[start:point])
+    pts = points.copy()
+    pts.append(len(temp))
+
+    for point in pts:
+        b_0, b_1 = estimate_coef(range(start, point), temp[start:point])
         coef.append((b_0, b_1))
         start = point
 
@@ -36,11 +40,24 @@ def estimate_point(coef, i):
 def estimate_all_points(coef, points, temp_size):
     estimated = []
     start = 0
-    points.append(temp_size)
 
-    for point, pos in points:
+    pts = points.copy()
+    pts.append(temp_size)
+
+    for pos, point in enumerate(pts):
         for i in range(start, point):
-            estimated.append(estimate_point(coef[pos - 1], i))
+            estimated.append(estimate_point(coef[pos], i))
         start = point
 
     return estimated
+
+
+if __name__ == "__main__":
+    temp = [4, 3, 2, 1, 7, 3, 1, 2, 5, 7, 1, 3, 7]
+    points = [3, 6, 10]
+
+    coef = estimate_all_coef(temp, points)
+    print("Coeficients:", coef)
+
+    estimated = estimate_all_points(coef, points, len(temp))
+    print("Estimated points:", estimated)
