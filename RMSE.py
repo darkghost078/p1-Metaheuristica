fix: Correct RMSE calculation and remove remove built-in random from requirements

RMSE.py
@@ -1,10 +1,12 @@
 from sklearn.metrics import mean_squared_error
+import numpy as np
 
 def RMSE (y_real,y_predict):
-    return mean_squared_error(y_real,y_predict)
+    mse = mean_squared_error(y_real,y_predict)
+    return np.sqrt(mse)
 
-
-a = [1,5,8,9,65,80]
-y = [2,5,10,11,64,96]
+if __name__ == "__main__":
+    a = [1,5,8,9,65,80]
+    y = [2,5,10,11,64,96]
 
 print(f"RMSE: {RMSE(a,y)}")