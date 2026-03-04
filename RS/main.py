e5b5f555d0ffebf51c3d81a3c8060581e5af3b83
david martinez <daviddarkghost@gmail.com>
2026-03-04 16:07:34 +0100

    Feat: mean chart of each series added

--- RS/main.py
+++ RS/main.py
@@ -6,6 +6,24 @@ from busqueda_aleatoria import randomSearch
 import matplotlib.pyplot as plt
 from linear_regression import estimate_all_points, estimate_all_coef
 
+def mean(serie):
+    means = []
+    for TS in range(len(serie[0])):
+        mean_s=[]
+        for i in range(len(serie)):
+            for j in range(len(serie[0][TS])):
+                if i == 0:
+                    mean_s.append(serie[i][TS][j]['rmse'])
+                else:
+                    mean_s[j] += serie[i][TS][j]['rmse']
+        for i in range(len(mean_s)):
+            mean_s[i] /= len(serie)
+        means.append(mean_s)
+    
+    return means
+
+
+
 def main():
     start = int(input("Introduzca el numero de individuos iniciales: "))
     end = int(input("Introduzca el numero de individuos finales: "))
@@ -29,51 +47,57 @@ def main():
 
 
     #Listas para guardar iters y best de cada serie
-    series_iters=[]
-    series_bests=[]
-    series_times=[]
 
-    for TS in range(len(series)):
-        times=[]
-        iters=[]
-        bests=[]
-        current_serie=series[TS]
-        current_k=k_values[TS]
-        for i in range(start,end,increment):
-            if i==start:
-                best, time, _= randomSearch(current_serie,current_k,increment)
-            else:
-                best, time, _= randomSearch(current_serie,current_k,increment,best)
+    mean_iters=[]
+    mean_bests=[]
+    mean_times=[]
+    for _ in range (20):
+        series_iters=[]
+        series_bests=[]
+        series_times=[]
+        for TS in range(len(series)):
+            times=[]
+            iters=[]
+            bests=[]
+            current_serie=series[TS]
+            current_k=k_values[TS]
+            for i in range(start,end,increment):
+                if i==start:
+                    best, time, _= randomSearch(current_serie,current_k,increment)
+                else:
+                    best, time, _= randomSearch(current_serie,current_k,increment,best)
 
 
-            iters.append(i)
-            bests.append(best.copy())
-            times.append(time)
+                iters.append(i)
+                bests.append(best.copy())
+                times.append(time)
 
-        series_iters.append(iters)
-        series_bests.append(bests)
-        series_times.append(times)
+            series_iters.append(iters)
+            series_bests.append(bests)
+            series_times.append(times)
+        
+        mean_times.append(series_times)
+        mean_bests.append(series_bests)
+        mean_iters.append(series_iters)
+
 
 
 #Graficas
-    #Grafica Iters vs RMSE
+
+    mean_series=mean(mean_bests)
+
     for TS in range(len(series_times)):
-        rmses = [element["rmse"] for element in series_bests[TS]]
-        plt.plot(series_iters[TS],rmses,marker ='o')
-        plt.title(f"RS {files[TS][3:]}")
-        plt.xlabel("Tiempo")
-        plt.ylabel("Mejor RMSE")
-        plt.grid(True)
-        plt.savefig(f"{files[TS][3:]}_ItersvsRMSE.png", dpi=300, bbox_inches='tight')
-        plt.show()
+        plt.title(f"Mean {files[TS][3:]}")
+        plt.plot(series_iters[TS], mean_series[TS], color='blue', linewidth=1, label="medias")
+        plt.savefig(f"{files[TS][3:]}_mean.png", dpi=300, bbox_inches='tight')
         plt.close()
+
 
     #Grafica de puntos resultado
     for TS in range(len(series_times)):
         plt.title(f"RS {files[TS][3:]}")
         #Linea real
         plt.plot(series[TS], color='blue', linewidth=1, label="Datos reales")
-
         #Lineas verticales
         best=series_bests[TS][len(series_bests[TS])-1]['points']
         for i in range(len(best)):
