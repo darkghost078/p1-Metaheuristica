eead10e9ada6fab9dc10654b35df073886686d7a
David Martinez Molina <daviddarkghost@gmail.com>
2026-03-08 18:24:45 +0100

    Fix: better linear regression

--- HC/hill_climbing.py
+++ HC/hill_climbing.py
@@ -17,38 +17,61 @@ def read_serie(path):
     return [float(x) for x in contenido]
 
 
-def estimate_segment_coef(x, y):
-    x = np.array(x).reshape(-1, 1)
+
+def estimate_coef(x, y):
+    x = np.array(x)
     y = np.array(y)
-    if len(x) < 2:
+    n = np.size(x)
+
+    if n == 0:
         return (0.0, 0.0)
-    model = LinearRegression().fit(x, y)
-    return (model.coef_[0], model.intercept_)
+
+    m_x = np.mean(x)
+    m_y = np.mean(y)
+
+    SS_xy = np.sum(y * x) - n * m_y * m_x
+    SS_xx = np.sum(x * x) - n * m_x * m_x
+
+    if abs(SS_xx) < 1e-8:
+        b_0 = 0.0
+        b_1 = m_y
+    else:
+        b_0 = SS_xy / SS_xx
+        b_1 = m_y - b_0 * m_x
+
+    return (float(b_0), float(b_1))
 
 
-def estimate_all_coef(serie, points):
+def estimate_all_coef(temp, points):
     coef = []
     start = 0
+
     pts = points.copy()
-    pts.append(len(serie))
-    for end in pts:
-        x = list(range(start, end))
-        y = serie[start:end]
-        coef.append(estimate_segment_coef(x, y))
-        start = end
+    pts.append(len(temp))
+
+    for point in pts:
+        b_0, b_1 = estimate_coef(range(start, point), temp[start:point])
+        coef.append((b_0, b_1))
+        start = point
+
     return coef
 
 
-def estimate_all_points(coef, points, n):
+def estimate_point(coef, i):
+    return i * coef[0] + coef[1]
+
+def estimate_all_points(coef, points, temp_size):
     estimated = []
     start = 0
+
     pts = points.copy()
-    pts.append(n)
-    for idx, end in enumerate(pts):
-        m, b = coef[idx]
-        for i in range(start, end):
-            estimated.append(m * i + b)
-        start = end
+    pts.append(temp_size)
+
+    for pos, point in enumerate(pts):
+        for i in range(start, point):
+            estimated.append(estimate_point(coef[pos], i))
+        start = point
+
     return estimated
 
 
@@ -108,12 +131,12 @@ def search_neighbour(serie, n):
     for i, point in enumerate(serie):
         # Mover + 1% del tamaño de la serie
         v1 = serie.copy()
-        v1[i] = point + int(n / 100)
+        v1[i] = point + int(n / 50)
         vecinos.append(v1)
 
         # Mover - 1% del tamaño de la serie
         v2 = serie.copy()
-        v2[i] = point - int(n / 100)
+        v2[i] = point - int(n / 50)
         vecinos.append(v2)
 
     validos = []
@@ -144,11 +167,11 @@ def hc(serie, k, best=None):
     solucion = randomPoints(len(serie), k)
     rmse = mean_rmse(serie, solucion)
     mejor_vecino = []
-
+    evolution=[]
     while mejor_vecino is not None:
         vecinos = search_neighbour(solucion, len(serie))
 
-        mejor_vecino = None
+        mejor_vecino = None 
         mejor_rmse = rmse
 
         for v in vecinos:
@@ -161,12 +184,14 @@ def hc(serie, k, best=None):
             solucion = mejor_vecino
             rmse = mejor_rmse
 
+        evolution.append({"rmse":rmse,"points":solucion})
+
     if best is not None and best["rmse"] < rmse:
         rmse = best["rmse"]
         solucion = best["points"]
 
     endTime = time.time()
-    return {"rmse": rmse, "points": solucion}, endTime - startTime
+    return {"rmse": rmse, "points": solucion}, endTime - startTime, evolution
 
 
 # ============================================================
@@ -186,7 +211,7 @@ def ejecutar_HC(series, k_values, repeticiones=20):
         rep_result = []
         for rep in range(repeticiones):
             if len(rep_result) > 0:
-                sol, tiempo = hc(
+                sol, tiempo, _= hc(
                     serie,
                     k,
                     {
@@ -195,7 +220,7 @@ def ejecutar_HC(series, k_values, repeticiones=20):
                     },
                 )
             else:
-                sol, tiempo = hc(serie, k)
+                sol, tiempo, _= hc(serie, k)
 
             rep_result.append(
                 {"rmse": sol["rmse"], "points": sol["points"], "tiempo": tiempo}
--- SA/simulated_annealing.py
+++ SA/simulated_annealing.py
@@ -32,30 +32,60 @@ def estimate_segment_coef(x, y):
     model = LinearRegression().fit(x, y)
     return (model.coef_[0], model.intercept_)
 
+def estimate_coef(x, y):
+    x = np.array(x)
+    y = np.array(y)
+    n = np.size(x)
 
-def estimate_all_coef(serie, points):
+    if n == 0:
+        return (0.0, 0.0)
+
+    m_x = np.mean(x)
+    m_y = np.mean(y)
+
+    SS_xy = np.sum(y * x) - n * m_y * m_x
+    SS_xx = np.sum(x * x) - n * m_x * m_x
+
+    if abs(SS_xx) < 1e-8:
+        b_0 = 0.0
+        b_1 = m_y
+    else:
+        b_0 = SS_xy / SS_xx
+        b_1 = m_y - b_0 * m_x
+
+    return (float(b_0), float(b_1))
+
+
+def estimate_all_coef(temp, points):
     coef = []
     start = 0
+
     pts = points.copy()
-    pts.append(len(serie))
-    for end in pts:
-        x = list(range(start, end))
-        y = serie[start:end]
-        coef.append(estimate_segment_coef(x, y))
-        start = end
+    pts.append(len(temp))
+
+    for point in pts:
+        b_0, b_1 = estimate_coef(range(start, point), temp[start:point])
+        coef.append((b_0, b_1))
+        start = point
+
     return coef
 
 
-def estimate_all_points(coef, points, n):
+def estimate_point(coef, i):
+    return i * coef[0] + coef[1]
+
+def estimate_all_points(coef, points, temp_size):
     estimated = []
     start = 0
+
     pts = points.copy()
-    pts.append(n)
-    for idx, end in enumerate(pts):
-        m, b = coef[idx]
-        for i in range(start, end):
-            estimated.append(m * i + b)
-        start = end
+    pts.append(temp_size)
+
+    for pos, point in enumerate(pts):
+        for i in range(start, point):
+            estimated.append(estimate_point(coef[pos], i))
+        start = point
+
     return estimated
 
 
@@ -109,12 +139,12 @@ def random_neighbour(points, n):
     for i, point in enumerate(points):
         # mover + 1% del tamaño de la serie
         v1 = points.copy()
-        v1[i] = point + int(n / 100)
+        v1[i] = point + int(n / 50)
         vecinos.append(v1)
 
         # mover - 1% del tamaño de la serie
         v2 = points.copy()
-        v2[i] = point - int(n / 100)
+        v2[i] = point - int(n / 50)
         vecinos.append(v2)
 
     # Filtrar vecinos inválidos
